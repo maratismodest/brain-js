fc13cdd803408294004e4c4d725f463ddcb1fd47
Marat Faizerakhmanov <maratismodest@gmail.com>
2024-06-09 15:43:53 +0300

    update

--- cars.js
+++ cars.js
@@ -2,59 +2,100 @@ const fs = require('fs');
 
 const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
+// LADA series
+// 1 = 1 000 000, 0.6 = 600 000 - keep in mind it's lux
 const models = [
     {
-        name: "Six",
-        value: 0.6
+        label: "Six",
+        price: 0.6,
     },
     {
-        name: 'Seven',
-        value: 0.7
+        label: 'Seven',
+        price: 0.7
     },
     {
-        name: 'Nine',
-        value: 0.8
+        label: 'Nine',
+        price: 0.8
     },
     {
-        name: 'Kalina',
-        value: 0.9
+        label: 'Kalina',
+        price: 1
     }
 ];
 
 // 2010 - 2024
-const years = [0.10, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20, 0.21, 0.22, 0.23, 0.24]
+// car price reduced depending on the age: 2015 car price lose 10% of factory price
+const years = [
+    {value: 0.80, label: '2010'},
+    {value: 0.82, label: '2011'},
+    {value: 0.84, label: '2012'},
+    {value: 0.86, label: '2013'},
+    {value: 0.88, label: '2014'},
+    {value: 0.90, label: '2015'},
+    {value: 0.92, label: '2016'},
+    {value: 0.94, label: '2017'},
+    {value: 0.96, label: '2018'},
+    {value: 0.99, label: '2019'}
+]
+
+// equipment: lux is the default, because we operate from 0 to 1 range
+const equipments = [
+    {
+        value: 0.79,
+        label: 'Base'
+    },
+    {
+        value: 0.89,
+        label: 'Comfort'
+    },
+    {
+        value: 0.99,
+        label: 'Lux'
+    }
+]
 
 const generateCars = (num) => {
     const cars = [];
     for (let i = 0; i < num; i++) {
+        const _model = getRandomElement(models).price
+        const _equipment = getRandomElement(equipments).value
+        const _year = getRandomElement(years).value
+        const _condition = 1 - Math.random() * Math.random()
+        const _mileage = Math.random() * Math.random()
+
         const input = {
-            model: getRandomElement(models).value,
-            year: getRandomElement(years),
-            condition: 1 - Math.random() * Math.random()
+            model: _model,
+            year: _year,
+            equipment: _equipment,
+            condition: _condition,
+            mileage: _mileage,
         };
 
-        const basePrice = 800000
-        const modelMultiplier = input.model
-        const yearMultiplier = (input.year + 0.24) * 2
-        const conditionMultiplier = input.condition
-        const random = Math.floor(Math.random() * 50000)
-        const price = Math.floor(basePrice * yearMultiplier * modelMultiplier * conditionMultiplier + random) / 1000000
+        const modelMultiplier = _model * 1000000
+        const yearMultiplier = _year
+        const conditionMultiplier = _condition
+        const mileageMultiplier = 1 - _mileage;
+        const equipmentMultiplier = _equipment
+        const priceVariety = Math.floor(Math.random() * 30000)
+        // const priceVariety = 0
+        const _price = Math.floor(modelMultiplier * yearMultiplier * equipmentMultiplier * mileageMultiplier * conditionMultiplier + priceVariety)
         const output = {
-            price
+            price: _price / 1000000
         }
         cars.push({input, output});
     }
     return cars;
 };
 
-const count = 1000
-const cars = generateCars(count);
-
-fs.writeFileSync('cars.json', JSON.stringify(cars, null, 2), 'utf-8');
-console.log(count + ' cars have been generated and saved to cars.json');
+// const count = 100000
+// const cars = generateCars(count);
+//
+// fs.writeFileSync('cars.json', JSON.stringify(cars, null, 2), 'utf-8');
+// console.log(count + ' cars have been generated and saved to cars.json');
 
 module.exports = {
     models,
     years,
+    equipments,
     getRandomElement
 }
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const brain = require('./brain-js');
 const data = require('./cars.json')
-const {models, getRandomElement, years} = require('./cars.js')
+const {models, getRandomElement, years, equipments} = require('./cars.js')
 
 // provide optional config object (or undefined). Defaults shown.
 const config = {
     // binaryThresh: 0.5,
-    hiddenLayers: [3], // array of integers for the sizes of the hidden layers in the network
+    // hiddenLayers: [3], // array of integers for the sizes of the hidden layers in the network
     // activation: 'sigmoid' // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh']
 };
 
@@ -13,18 +13,30 @@ const config = {
 const net = new brain.NeuralNetwork(config);
 net.train(data)
 
-const model = getRandomElement(models)
-const year = getRandomElement(years)
+for (let i = 0; i < equipments.length; i++) {
+    const prices = []
+    const _model = models[3]
+    const _equipment = equipments[i]
+    const _year = years[5]
+    const car = {
+        model: _model.price,
+        year: years[5].value,
+        equipment: _equipment.value,
+        condition: 0.94, // 1.00 is perfect condition
+        mileage: 0.094, // 94 000 miles
+    }
+    for (let i = 0; i < 10; i++) {
+        const output = net.run(car);
+        prices.push(output.price)
+    }
 
-const car = {
-    model: model.value,
-    year: year,
-    condition: 0.875
+    const price = Math.floor(prices.reduce((prev, item) => prev + item, 0)  /prices.length * 1000000)
+    const result = `The model ${_model.label} ${_equipment.label} of ${_year.label} year in ${car.condition} condition with ${car.mileage * 1000 * 1000} mileage costs ${price} RUB`
+
+    console.log(result)
 }
 
-const output = net.run(car);
 
-const price = Math.floor(output.price * 1000000)
 
-const result = `The model ${model.name} of ${car.year * 100 + 2000} year in ${car.condition} condition costs ${price} RUB`
-console.log(result)
+
+
